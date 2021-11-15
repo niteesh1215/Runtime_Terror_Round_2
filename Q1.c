5469923adc24e8300505ce5d87b6d525c8dada10
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-11-15 22:10:30 +0530

    .

--- Q1.c
+++ Q1.c
@@ -1,3 +1,18 @@
+/* 
+ Program to count number of 1 bit's in an integer
+ 
+
+Output:
+enter the integer
+7
+no of bits those are 1 in its binary representation: 3
+
+
+Explanation:
+7 in binary is 111
+Therefore 1 bit count is 3
+*/
+
 #include <stdio.h>
 
 int main()
